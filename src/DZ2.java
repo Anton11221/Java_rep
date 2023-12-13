94042383797e79d582092b9e3b2d4ad67ce89c64
Anton11221 <xxx-i@ya.ru>
2023-12-13 11:25:33 +0300

    Выполнил задание 1 и 2

---
+++ src/DZ2.java
@@ -0,0 +1,12 @@
+public class DZ2 {
+    public static void main(String[] args) {
+        // Создание переменной типа с плавающей точкой с наибольшим диапазоном чисел
+        double num1 = 123.12345;
+
+        // Создание переменной целочисленного типа с наименьшим диапазоном чисел
+        byte num2 = 127;
+
+        System.out.println("Значение num1: " + num1);
+        System.out.println("Значение num2: " + num2);
+    }
+}
