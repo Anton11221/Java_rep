7b5e0192b8eb5d03fa4e39804620878b514bad4a
Anton11221 <xxx-i@ya.ru>
2023-12-13 22:03:09 +0300

    Добавил задание №3

--- src/DZ3.java
+++ src/DZ3.java
@@ -2,15 +2,14 @@ public class DZ3 {
     public static void main(String[] args) {
         int[] num = {1, 2, 5, 4, 5};
 
-        // Поменять местами первый и последний элемент
+        // Меняем местами первый и последний элемент
         int temp = num[0];
         num[0] = num[num.length - 1];
         num[num.length - 1] = temp;
 
-        // Вычислить сумму первого и среднего элемента
+        // Вычисляем сумму первого и среднего элемента
         int sum = num[0] + num[num.length / 2];
 
-        // Вывести результат в консоль
         System.out.println("Сумма первого и среднего элемента: " + sum);
     }
 }
