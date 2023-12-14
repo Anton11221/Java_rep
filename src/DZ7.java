29a7275806db7d864e636207e92c12d031c13a7c
Anton11221 <xxx-i@ya.ru>
2023-12-15 02:22:51 +0300

    Добавил решение задания №7

---
+++ src/DZ7.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DZ7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод размера массива
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        // Ввод элементов массива
+        int[] array = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // Сравнение элементов массива с константами
+        int x = 10;
+        int y = 20;
+        int z = 30;
+        boolean containsConstant = false;
+        for (int i = 0; i < size; i++) {
+            if (array[i] == x || array[i] == y || array[i] == z) {
+                System.out.println("Массив содержит значение: " + array[i]);
+                containsConstant = true;
+            }
+        }
+
+        // Вывод результата
+        if (containsConstant) {
+            System.out.println("Данное значение имеется в константах");
+        } else
+            System.out.println("Константы в массиве не найдены");
+    }
+}
