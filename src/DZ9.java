72bdf08b101ecd0bc635c2450f3c118e9751af1b
Anton11221 <xxx-i@ya.ru>
2023-12-15 03:21:20 +0300

    Добавил решение задания №9

---
+++ src/DZ9.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class DZ9 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод размера массива
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        // Ввод элементов массива
+        double[] array = new double[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextDouble();
+        }
+
+        // Вычисление среднего арифметического
+        double sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += array[i];
+        }
+        double average = sum / size;
+
+        // Умножение элементов массива на среднее арифметическое
+        for (int i = 0; i < size; i++) {
+            array[i] *= average;
+        }
+
+        // Вывод массива на экран
+        System.out.println("Массив, умноженный на среднее арифметическое:");
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
