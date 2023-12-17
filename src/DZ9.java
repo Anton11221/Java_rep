c3828f4a29e0be1956ab3d60d352517683c18941
Anton11221 <xxx-i@ya.ru>
2023-12-17 20:14:20 +0300

    Добавил решение задания №9

--- src/DZ9.java
+++ src/DZ9.java
@@ -21,6 +21,7 @@ public class DZ9 {
             sum += array[i];
         }
         double average = sum / size;
+        System.out.println("Среднее арефметическое равно = " + average);
 
         // Умножение элементов массива на среднее арифметическое
         for (int i = 0; i < size; i++) {
