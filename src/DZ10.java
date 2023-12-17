3cc9966dc696d72b17b60cc25e992e6eabbab280
Anton11221 <xxx-i@ya.ru>
2023-12-17 20:35:59 +0300

    Добавил решение задания №10

---
+++ src/DZ10.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DZ10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод размерности матрицы
+        System.out.print("Введите количество строк: ");
+        int rows = scanner.nextInt();
+        System.out.print("Введите количество столбцов: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        // Ввод элементов матрицы
+        System.out.println("Введите элементы матрицы:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Первая строка матрицы, умноженная на 3:");
+        for (int j = 0; j < columns; j++) {
+            System.out.print(matrix[0][j] * 3 + " ");
+        }
+    }
+}
